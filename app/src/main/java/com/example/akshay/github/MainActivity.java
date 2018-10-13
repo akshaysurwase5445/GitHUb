779799e852b006822fcc8383e39ca5664130ed3c
akshay surwase <akshaysurwase189@gmail.com>
2018-10-13 15:32:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/akshay/github/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.akshay.github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void doSomeWork(){
+
+        int i=11;
+        int k=66;
+
+
+        if(i<=100){
+
+        }
+
+    }
+}
